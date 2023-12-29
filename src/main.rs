8f329c45151ea3d2e55b01578a39be07a7690c16
zbrachinara <zbrachinara@gmail.com>
2023-12-29 07:51:48 -0500

    merge plugins in main fn

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,13 @@ use stack_practice::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(AssetPlugin {
-            watch_for_changes_override: Some(false),
-            ..default()
-        }))
-        .add_plugins(assets::StackingAssetsPlugin)
-        .add_plugins(board::BoardPlugin)
-        .add_plugins(state::StatePlugin)
+        .add_plugins((
+            DefaultPlugins.set(AssetPlugin {
+                watch_for_changes_override: Some(false),
+                ..default()
+            }),
+            assets::StackingAssetsPlugin,
+            board::BoardPlugin,
+        ))
         .run();
 }
