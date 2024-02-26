0d6980c411e4543061dc4639ffa45ffbdef9d5bf
zbrachinara <zbrachinara@gmail.com>
2024-02-26 15:29:33 -0500

    various update refactoring

--- src/board/update.rs
+++ src/board/update.rs
@@ -2,7 +2,7 @@ use std::ops::Deref;
 
 use bevy::math::ivec2;
 use bevy::prelude::*;
-use tap::Tap;
+use tap::{Tap, TapOptional};
 
 use crate::assets::tables::{
     kick_table::{KickParameters, KickTable},
@@ -58,7 +58,7 @@ impl<'world> BoardQueryItem<'world> {
     /// Starting from zero, finds the highest number for which the associated mino (given by `f`) is
     /// within the bounds of the matrix. If there is no such number, `None` is returned. Otherwise,
     /// the value will always be a non-negative number.
-    fn maximum_for_which<F>(&self, table: &ShapeTable, mut f: F) -> Option<i32>
+    fn maximum_valid<F>(&self, table: &ShapeTable, mut f: F) -> Option<i32>
     where
         F: FnMut(i32) -> Mino,
     {
@@ -67,16 +67,21 @@ impl<'world> BoardQueryItem<'world> {
             .and_then(|o| (o > 0).then_some(o - 1))
     }
 
+    fn drop_height(&mut self, shape_table: &QueryShapeTable, active: Mino) -> i32 {
+        self.maximum_valid(shape_table, |y| active.tap_mut(|p| p.position.y -= y))
+            .unwrap()
+    }
+
     /// If the controller requests that the active piece is shifted, the piece will be shifted and
     /// marked as modified. Returns true if the shift was successful.
-    fn do_shift(&mut self, controller: &Controller, shape_table: &ShapeTable) -> bool {
+    fn shift(&mut self, controller: &Controller, shape_table: &ShapeTable) -> bool {
         let farthest_shift_left = -self
-            .maximum_for_which(shape_table, |x| {
+            .maximum_valid(shape_table, |x| {
                 self.active().tap_mut(|p| p.position.x -= x)
             })
             .unwrap();
         let farthest_shift_right = self
-            .maximum_for_which(shape_table, |x| {
+            .maximum_valid(shape_table, |x| {
                 self.active().tap_mut(|p| p.position.x += x)
             })
             .unwrap();
@@ -85,15 +90,14 @@ impl<'world> BoardQueryItem<'world> {
             .shift
             .clamp(farthest_shift_left, farthest_shift_right);
 
-        if shift_size != 0 {
-            self.active_mut().position.x += shift_size;
-            true
-        } else {
-            false
-        }
+        (shift_size != 0).tap(|&shifting| {
+            if shifting {
+                self.active_mut().position.x += shift_size;
+            }
+        })
     }
 
-    fn do_rotate(
+    fn rotate(
         &mut self,
         controller: &Controller,
         kick_table: &KickTable,
@@ -126,25 +130,27 @@ impl<'world> BoardQueryItem<'world> {
             })
             .find(|m| has_free_space(self.matrix.deref(), *m, shape_table));
 
-        if let Some(successful_rot) = successful_rot {
-            *self.active_mut() = successful_rot;
-            return true;
-        }
-
-        false
+        successful_rot
+            .tap_some(|&rot| {
+                *self.active_mut() = rot;
+            })
+            .is_some()
     }
 
     /// Switches the held piece and the active piece, if it is allowed. By this point, the active
     /// piece must exist.
     fn switch_hold_active(&mut self) -> Option<MinoKind> {
-        if let &Hold::Ready(p) = self.hold.deref() {
-            *(self.hold) = Hold::Inactive(self.take_active().kind);
-            Some(p)
-        } else if matches!(self.hold.deref(), Hold::Empty) {
-            *(self.hold) = Hold::Inactive(self.take_active().kind);
-            Some(self.queue.take())
-        } else {
-            None
+        match self.hold.deref() {
+            Hold::Empty => {
+                *(self.hold) = Hold::Inactive(self.take_active().kind);
+                Some(self.queue.take())
+            }
+            Hold::Ready(piece) => {
+                let piece = *piece;
+                *(self.hold) = Hold::Inactive(self.take_active().kind);
+                Some(piece)
+            }
+            Hold::Inactive(_) => None,
         }
     }
 
@@ -157,13 +163,12 @@ impl<'world> BoardQueryItem<'world> {
 
     /// Attempts to spawn the given piece on the board, returning whether spawning was successful.
     pub fn spawn_piece(&mut self, piece: Mino, shape_table: &ShapeTable) -> bool {
-        if has_free_space(&self.matrix, piece, shape_table) {
-            *self.drop_clock = default();
-            self.active.0 = Some(piece);
-            true
-        } else {
-            false
-        }
+        has_free_space(&self.matrix, piece, shape_table).tap(|&has_free_space| {
+            if has_free_space {
+                *self.drop_clock = default();
+                self.active.0 = Some(piece);
+            }
+        })
     }
 }
 
@@ -192,9 +197,7 @@ pub(crate) fn update_board(
 
         if controller.hard_drop {
             let mut active = board.take_active();
-            let farthest_legal_drop = board
-                .maximum_for_which(&shape_table, |y| active.tap_mut(|p| p.position.y -= y))
-                .unwrap();
+            let farthest_legal_drop = board.drop_height(&shape_table, active);
             active.position.y -= farthest_legal_drop;
             lock_piece(&mut board.matrix, active, &shape_table);
             let new_piece = board.queue.take();
@@ -205,11 +208,7 @@ pub(crate) fn update_board(
             continue;
         }
 
-        let farthest_legal_drop = board
-            .maximum_for_which(&shape_table, |y| {
-                board.active().tap_mut(|p| p.position.y -= y)
-            })
-            .unwrap();
+        let farthest_legal_drop = board.drop_height(&shape_table, board.active());
 
         // The drop clock should only either drop the piece or lock it, NOT BOTH. This is so
         // that the player has time to interact with the piece when it hits the bottom, for a
@@ -242,8 +241,8 @@ pub(crate) fn update_board(
             }
         }
 
-        let rotation_success = board.do_rotate(&controller, &kick_table, &shape_table);
-        let shift_success = board.do_shift(&controller, &shape_table);
+        let rotation_success = board.rotate(&controller, &kick_table, &shape_table);
+        let shift_success = board.shift(&controller, &shape_table);
 
         if rotation_success || shift_success {
             // TODO also modify a lock reset counter
